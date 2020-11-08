fc66697fcf44680f0b5c8a2fd7f61f8137c3b2ac
Antoni Angga <antoniangga14@gmail.com>
2020-11-08 21:02:59 +0700

    get by id coffees

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees') /* this is routes and contoller naming */
 export class CoffeesController {
+  // GET ALL here
   @Get()
   findAll() {
     return 'This Action returns all coffeess';
+  }
+
+  // GET By id here
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This Action return params ID ${id}`;
   }
 }
